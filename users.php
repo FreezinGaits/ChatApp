8554a100dcb3e8c37478a18c2f1fdd0d184dd34d
FreezinGaits <freezinggaits917@gmail.com>
2025-04-05 11:53:01 +0530

    new

--- users.php
+++ users.php
@@ -8,6 +8,47 @@
 <?php include_once "header.php"; ?>
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        .home-btn {
+            display: inline-block;
+            padding: 8px 15px;
+            background-color: #4CAF50;
+            color: white;
+            text-decoration: none;
+            border-radius: 4px;
+            font-size: 14px;
+            border: none;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            margin-right: 10px; /* Added spacing between buttons */
+        }
+
+        .home-btn:hover {
+            background-color: #45a049;
+        }
+
+        .home-btn i {
+            margin-right: 5px;
+        }
+
+        .header-buttons {
+            display: flex;
+            align-items: center;
+        }
+
+        .logout {
+            padding: 8px 15px;
+            background-color: #f44336;
+            color: white;
+            text-decoration: none;
+            border-radius: 4px;
+            font-size: 14px;
+        }
+
+        .logout:hover {
+            background-color: #da190b;
+        }
+    </style>
 </head>
 
 <body>
@@ -27,10 +68,15 @@
             <p><?php echo $row['status']; ?></p>
           </div>
         </div>
-        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+        <div class="header-buttons">
+        <a href="http://127.0.0.1:5503/index.html?logged_in=true" class="home-btn">
+  <i class="fas fa-home"></i> Home
+</a>
+          <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+        </div>
       </header>
       <div class="search">
-        <span class="text">Select an user to start chat</span>
+        <span class="text">Select a user to start chat</span>
         <input type="text" placeholder="Enter name to search...">
         <button><i class="fas fa-search"></i></button>
       </div>
